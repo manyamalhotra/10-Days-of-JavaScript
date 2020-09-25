6b63b992346b403cae57a592d625c7cd36c4c06c
manyamalhotra <61011560+manyamalhotra@users.noreply.github.com>
2020-09-25 06:07:08 +0530

    Create Day 2: Loops.js

---
+++ Day 2/Day 2: Loops.js
@@ -0,0 +1,46 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    let vowels = "aeiou";
+    var consonants = "";
+
+    for(let v of s) {
+        if(vowels.includes(v))
+            console.log(v);
+        else
+            consonants += v + "\n";
+    }
+    console.log(consonants);
+
+
+function main() {
+    const s = readLine();
+    
+    vowelsAndConsonants(s);
+}
